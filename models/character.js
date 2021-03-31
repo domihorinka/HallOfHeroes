08cd6ffc8ad99d6ce56983e541db67d8e827b271
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-03-31 13:46:15 -0600

    Worked on the seeds and the schema

--- models/character.js
+++ models/character.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const characterSchema = new Schema({
     name: {
         type:String,
@@ -15,8 +14,50 @@ const characterSchema = new Schema({
         type:Number,
         required: true
     },
+    hp: {
+        type: Number,
+        required: true
+    },
+    stats: [{
+        strength: {
+            type: Number,
+            required: true
+        },
+        dexterity: {
+            type: Number,
+            required: true
+        },
+        constitution: {
+            type: Number,
+            required: true
+        },
+        intelligence: {
+            type: Number,
+            required: true
+        },
+        wisdom: {
+            type: Number,
+            required: true
+        },
+        charisma: {
+            type: Number,
+            required: true
+        },
+        charisma: {
+            type: Number,
+            required: true
+        },
+    }],
+    inventory: {
+        type: String,
+    },
+    background: {
+        type: String,
+    },
+    appearance: {
+        type: String,
+    },
 });
 
-
-const character = mongoose.model("Character",characterSchema);
+const character = mongoose.model("character",characterSchema);
 module.exports = character;
