4181d031d489ed3f823068c4cf95faabb8c3e3f8
domihorinka <domihorinka@gmail.com>
2021-04-01 14:26:23 -0600

    fixed card things, need to fix further

--- client/src/components/Cards/Card.js
+++ client/src/components/Cards/Card.js
@@ -1,4 +1,30 @@
 import React from 'react'
-import Card from 'react-bootstrap/Card'
 import './Card.css'
+import { CardView } from 'react-card-with-image'
+import littlebirddude from '../../images/littlebirddude.png'
+
+const Card = (props) => {
+    const items = [
+      {
+        id: 1,
+        name: {props.name},
+        class: {props.class},
+        level: {props.level},
+        image: littlebirddude
+      }
+    ]
+    return (
+      <CardView
+        items={items}
+        activeColor='#000'
+        imageHeight='650px'
+        imageWidth='800px'
+      />
+    )
+  }
+
+
+
+
+export default Card
 
--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,58 +1,21 @@
 import React from 'react'
-import { CardView } from 'react-card-with-image'
-import 'react-card-with-image/dist/index.css'
-import littlebirddude from '../images/littlebirddude.png'
-// import dragondude from '../images/dragondude.png'
-
-const List1 = () => {
-    const items = [
-      {
-        id: 1,
-        header: 'Lorem ipsum',
-        description:
-          'dolor sit amet, consectetur adipiscing elit. Sed tempus nunc et tincidunt lobortis. Aliquam placerat, justo sit amet mattis molestie, ipsum nisi congue turpis, in imperdiet nisi nisl sit amet arcu. Donec euismod eu ante quis elementum. Maecenas commodo erat',
-        image: littlebirddude
-      },
-      {
-        id: 2,
-        header: 'Sed cursus',
-        description:
-          'in metus quis tempor. Donec at venenatis magna, vel fringilla dui. Curabitur id gravida ipsum. Donec at mollis massa. Nullam metus elit, pret',
-        image: littlebirddude
-      },
-      {
-        id: 3,
-        header: 'Sed fermentum',
-        description:
-          'condimentum purus, non sagittis massa faucibus id. Sed finibus convallis lectus eu fringilla. Proin lacinia sem vitae nunc consectetur, a faucibus orci ultricie',
-        image: littlebirddude
-      },
-      {
-        id: 4,
-        header: 'Proin et urna',
-        description:
-          'vitae neque fermentum fringilla. Proin bibendum sollicitudin aliquet. Fusce id magna aliquam, pulvinar metus vitae, bibendum felis.',
-        image: littlebirddude
-      },
-      {
-        id: 5,
-        header: 'Cras leo velit',
-        description:
-          'finibus id eros eu, commodo sollicitudin lacus. Nunc semper enim nec est viverra, at pharetra orci lobortis. Nulla facilisi. Sed non lectus nunc.',
-        image: littlebirddude
-      }
-    ]
-    return (
-      <CardView
-        items={items}
-        activeColor='#000'
-        imageHeight='650px'
-        imageWidth='800px'
-      />
-    )
-  }
+// import 'react-card-with-image/dist/index.css'
+import Card from '../components/Cards/Card'
+import seed from '../../../scripts/seed.json'
 
 
+function List() {
+  return (
+      <>
+          <Card 
+          name={seed[0].name}
+          class={seed[0].class}
+          level={seed[0].level}
+          image={seed[0].image}
+          />
 
-
-export default List1
+      
+      </>
+  )
+}
+export default List
