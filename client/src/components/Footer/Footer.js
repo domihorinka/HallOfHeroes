233c9d8335e7b0dac2560d42028d42ae49a75288
Marble <mrivera0014@gmail.com>
2021-04-01 23:52:53 -0600

    created and styled footer element

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,10 +7,15 @@ function Footer() {
             {/* <div className='phantom' /> */}
             <footer className='footer'>
                 <p className="footerDes">An application created by University of Denver bootcamp student to create and store characters.</p>
-
-                <ul>
-                    <li className="group" ><a target="_blank" rel="noreferrer" href="https://github.com/mrivera0014">Maribel Rivera</a></li>
-                </ul>
+                <div className="group">
+                    <h4>Contributors</h4>
+                    <ul>
+                        <li  ><a target="_blank" rel="noreferrer" href="https://github.com/domihorinka">Dominique Horinka</a></li>
+                        <li  ><a target="_blank" rel="noreferrer" href="https://github.com/AlbertTundy">Stephen Pacyga</a></li>
+                        <li  ><a target="_blank" rel="noreferrer" href="https://github.com/mrivera0014">Maribel Rivera</a></li>
+                        <li  ><a target="_blank" rel="noreferrer" href="https://github.com/Tevvels">Christopher Watkins</a></li>
+                    </ul>
+                </div>
             </footer>
         </>
     )
--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,21 +1,21 @@
 import React from 'react'
-// import 'react-card-with-image/dist/index.css'
+import 'react-card-with-image/dist/index.css'
 import Card from '../components/Cards/Card'
 import seed from '../../../scripts/seed.json'
 
 
 function List() {
   return (
-      <>
-          <Card 
-          name={seed[0].name}
-          class={seed[0].class}
-          level={seed[0].level}
-          image={seed[0].image}
-          />
+    <>
+      <Card
+        name={seed[0].name}
+        class={seed[0].class}
+        level={seed[0].level}
+        image={seed[0].image}
+      />
 
-      
-      </>
+
+    </>
   )
 }
 export default List
