32835d75a3abd0eb4d59a8e93c80b29accaa646f
Marble <mrivera0014@gmail.com>
2021-04-05 21:05:36 -0600

    modal displaying again, need to fix so only cancel buttons closes modal

--- client/src/pages/Question.js
+++ client/src/pages/Question.js
@@ -1,90 +1,92 @@
 
-import React ,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Questions() {
+
+function Questions(onCancel) {
 
 
     const questions = [
         {
             questionText: `Welcome {Adventurer} to Dungeons and Dragons!, How exciting you decided to join us. lets start by making a character the best to do that to pick a role. what would you like to do?`,
             possibleAnswers: [
-                {answerText:"Strong",answerValue:"Str" },
-                {answerText:"Nimble",answerValue:"Dex" },
-                {answerText:"Mystical",answerValue:"Int" }
+                { answerText: "Strong", answerValue: "Str" },
+                { answerText: "Nimble", answerValue: "Dex" },
+                { answerText: "Mystical", answerValue: "Int" }
             ]
         },
         {
             questionText: `Excellent choice {Adventurer} now being from what classes would you wanna play?`,
             possibleAnswers: [
-                 {answerText: "Barbarian", answerValue:"barbarian"},       
-                 {answerText: "fighter", answerValue:"fighter"},       
-                 {answerText: "paladin", answerValue:"paladin"}    
+                { answerText: "Barbarian", answerValue: "barbarian" },
+                { answerText: "fighter", answerValue: "fighter" },
+                { answerText: "paladin", answerValue: "paladin" }
             ]
-        
+
         },
         {
             questionText: `Excellent choice {Adventurer} now being from what classes would you wanna play?`,
             possibleAnswers: [
-                 {answerText: "rogue", answerValue:"rogue"},       
-                 {answerText: "ranger", answerValue:"ranger"},       
-                 {answerText: "monk", answerValue:"monk"}    
+                { answerText: "rogue", answerValue: "rogue" },
+                { answerText: "ranger", answerValue: "ranger" },
+                { answerText: "monk", answerValue: "monk" }
             ]
         },
         {
             questionText: `Excellent choice {Adventurer} now being from what classes would you wanna play?`,
             possibleAnswers: [
-                 {answerText: "sorcerer", answerValue:"sorcerer"},       
-                 {answerText: "wizard", answerValue:"wizard"},       
-                 {answerText: "warlock", answerValue:"warlock"},    
-    
+                { answerText: "sorcerer", answerValue: "sorcerer" },
+                { answerText: "wizard", answerValue: "wizard" },
+                { answerText: "warlock", answerValue: "warlock" },
+
             ]
         },
         {
             questionText: `a fine choice indeed playing a {} with just great {}! now what name should there character have?`,
             possibleAnswers: [
-                 {answerText: "sorcerer", answerValue:"sorcerer"},       
-                 {answerText: "wizard", answerValue:"wizard"},       
-                 {answerText: "warlock", answerValue:"warlock"}    
+                { answerText: "sorcerer", answerValue: "sorcerer" },
+                { answerText: "wizard", answerValue: "wizard" },
+                { answerText: "warlock", answerValue: "warlock" }
             ]
         }
     ]
 
 
-    const [currentQuestion,setCurrentQuetion] = useState(0);
-    const [showCharacter,setShowCharacter] = useState([]
+    const [currentQuestion, setCurrentQuetion] = useState(0);
+    const [showCharacter, setShowCharacter] = useState([]);
+    const [showModal, setShowModal] = useState(false)
 
-    );
+
     let nextQuestion;
     let numbernew;
     let makeCharacter;
-    const handleButtonClicked = (option) =>{
+    const handleButtonClicked = (option) => {
 
-        if(option == "Dex"){
+        if (option == "Dex") {
             numbernew = 2;
-            makeCharacter = {...showCharacter, Prof: option};
+            makeCharacter = { ...showCharacter, Prof: option };
         }
-       else if(option == "Str"){
+        else if (option == "Str") {
             numbernew = 1;
-            makeCharacter = {...showCharacter, Prof: option};
+            makeCharacter = { ...showCharacter, Prof: option };
         }
-        else if(option == "Int"){
+        else if (option == "Int") {
             numbernew = 3;
-            makeCharacter = {...showCharacter, Prof: option};
+            makeCharacter = { ...showCharacter, Prof: option };
         }
         else {
-            makeCharacter = {...showCharacter, class: option};
+            makeCharacter = { ...showCharacter, class: option };
 
         }
         nextQuestion = currentQuestion + numbernew;
 
-        
-        
-        if(nextQuestion < questions.length){
-        setCurrentQuetion(nextQuestion)
-        setShowCharacter(makeCharacter)
+
+
+        if (nextQuestion < questions.length) {
+            setCurrentQuetion(nextQuestion)
+            setShowCharacter(makeCharacter)
         } else {
             console.log(makeCharacter)
-        
+
         }
     }
 
@@ -97,8 +99,9 @@ function Questions() {
                 {questions[currentQuestion].questionText}
             </div>
             <div>
-            {questions[currentQuestion].possibleAnswers.map(( possibleAnswer ) => {return (<button onClick={()=>{handleButtonClicked(possibleAnswer.answerValue)}}> {possibleAnswer.answerText} </button>)})  }
+                {questions[currentQuestion].possibleAnswers.map((possibleAnswer) => { return (<button onClick={() => { handleButtonClicked(possibleAnswer.answerValue) }}> {possibleAnswer.answerText} </button>) })}
             </div>
+            <button onClick={onCancel}>Cancel</button>
         </div>
     )
 }
