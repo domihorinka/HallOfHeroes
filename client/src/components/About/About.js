3322b97c4aa97e2f3a98ffd30816bc71d8d1067f
Marble <mrivera0014@gmail.com>
2021-04-05 23:54:31 -0600

    resolved warnings

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -10,7 +10,7 @@ function About() {
                 <p className='desc'>
                     Hall of Heroes is a web application that anyone can use to create and store their Dungeons and Dragons characters.
                 </p>
-                <h3 class='what'>What is Dungeons and Dragons?</h3>
+                <h3 className='what'>What is Dungeons and Dragons?</h3>
                 <p className='desc'>
                     Dungeons and Dragons is a RPG game where one player, the DM, runs the campaign in which the other players will create characters who will be adventurers in the campaign. The adventurers will have to navigate through the campaign as other characters, monsters, and terrains that the DM will create to challenge the adventurers. As the adventurers face and defeat bosses the players will gain experience and become stronger, it is up to the players to keep role play and keep their characters alive.
                 </p>
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from "react-router-dom"; 
-import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Auth0Provider} from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -13,11 +12,11 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 ReactDOM.render(
   <Router>
     <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}>
-    
-    <App />
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}>
+
+      <App />
     </Auth0Provider>
   </Router>,
   document.getElementById('root')
